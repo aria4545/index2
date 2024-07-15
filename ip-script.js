cbdaa4bafa7fffbd46a6806e9700f45eee0b4923
aria4545 <162304629+aria4545@users.noreply.github.com>
2024-07-15 21:34:37 +0300

    Add files via upload

---
+++ ip-script.js
@@ -0,0 +1,9 @@
+const getlocation = () => {
+    // get the user location with ip address
+    fetch("https://ipapi.co/json")
+         .then((response) => response.json())
+         .then((data) => {
+            const des = document.querySelector("p");
+            des.innerHTML = 'Latitude: ' + data.latitude + ' Longitude: ' + data.longitude;
+         });
+};
